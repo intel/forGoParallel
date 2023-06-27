00b8bb9aa914a9ef28ce952444c48dca06cf3237
Pascal Costanza <pascal.costanza@intel.com>
2023-06-27 11:47:39 +0200

    Additional map functions.

--- gsync/sync.go
+++ gsync/sync.go
@@ -8,6 +8,14 @@ import (
 // Map is a type-safe version of sync.Map.
 type Map[K comparable, V any] sync.Map
 
+func (m *Map[K, V]) CompareAndDelete(key K, value V) (deleted bool) {
+	return (*sync.Map)(m).CompareAndDelete(key, value)
+}
+
+func (m *Map[K, V]) CompareAndSwap(key K, old, new V) bool {
+	return (*sync.Map)(m).CompareAndSwap(key, old, new)
+}
+
 func (m *Map[K, V]) Delete(key K) {
 	(*sync.Map)(m).Delete(key)
 }
@@ -41,6 +49,14 @@ func (m *Map[K, V]) Range(f func(K, V) bool) {
 
 func (m *Map[K, V]) Store(key K, value V) {
 	(*sync.Map)(m).Store(key, value)
+}
+
+func (m *Map[K, V]) Swap(key K, value V) (previous V, loaded bool) {
+	prev, loaded := (*sync.Map)(m).Swap(key, value)
+	if loaded {
+		previous = prev.(V)
+	}
+	return
 }
 
 // Pool is a type-safe version of sync.Pool.
